705e93bc96b8ba4e052ad0871d17cd9d9cd0518a
Olamilekan <olamilekan.akinlusi@icloud.com>
2025-03-17 12:45:04 -0500

    exploreitems dynamic

--- src/components/explore/ExploreItems.jsx
+++ src/components/explore/ExploreItems.jsx
@@ -1,25 +1,100 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import AuthorImage from "../../images/author_thumbnail.jpg";
-import nftImage from "../../images/nftImage.jpg";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from 'axios';
+import SkeletonNftExplore from "../UI/SkeletonNftExplore";
 
 const ExploreItems = () => {
+  const [localNftData, setLocalNftData] = useState([]);
+  const [visibleItems, setVisibleItems] = useState(8);
+  const [timeLeft, setTimeLeft] = useState(5 * 60 * 60 + 30 * 60 + 32);
+  const [isLoading, setIsLoading] = useState(true);
+  const [filter, setFilter] = useState("");
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (localNftData && localNftData.length > 0) {
+      setIsLoading(false);
+    }
+  }, [localNftData]);
+
+  useEffect(() => {
+    const fetchNFTs = async () => {
+      try {
+        const response = await axios.get(
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/explore?filter=${filter}`
+        );
+        setLocalNftData(response.data);
+      } catch (error) {
+        console.error("There was an error fetching the NFT data!", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchNFTs();
+
+    const timeout = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+
+  }, [filter]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime <= 0) {
+          clearInterval(interval);
+
+          return 0;
+        }
+
+        return prevTime - 1;
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [timeLeft]);
+
+  const hours = Math.floor(timeLeft / 3600);
+
+  const minutes = Math.floor((timeLeft % 3600) / 60);
+
+  const seconds = timeLeft % 60;
+
+  const loadMore = () => {
+    setVisibleItems(prevVisibleItems => prevVisibleItems + 4);
+  };
+
   return (
     <>
       <div>
-        <select id="filter-items" defaultValue="">
+        <select id="filter-items" value={filter} onChange={(e) => setFilter(e.target.value)}>
           <option value="">Default</option>
           <option value="price_low_to_high">Price, Low to High</option>
           <option value="price_high_to_low">Price, High to Low</option>
           <option value="likes_high_to_low">Most liked</option>
         </select>
       </div>
-      {new Array(8).fill(0).map((_, index) => (
-        <div
-          key={index}
-          className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
-          style={{ display: "block", backgroundSize: "cover" }}
-        >
+
+      {isLoading ? (
+            <div className="row">
+              {Array.from({ length: 4 }).map((_, index) => (
+                <div
+                  className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+                  key={index}
+                >
+                  <SkeletonNftExplore />
+                </div>
+              ))}
+            </div>
+          ) : (
+            <>
+      {localNftData.length > 0 &&
+   localNftData.slice(0, visibleItems).map((nft, index) => (
+        <div key={index}
+        className="d-item col-lg-3 col-md-6 col-sm-6 col-xs-12"
+        style={{ display: "block", backgroundSize: "cover" }}>
           <div className="nft__item">
             <div className="author_list_pp">
               <Link
@@ -27,11 +102,11 @@ const ExploreItems = () => {
                 data-bs-toggle="tooltip"
                 data-bs-placement="top"
               >
-                <img className="lazy" src={AuthorImage} alt="" />
+                <img className="lazy" src={nft.authorImage} alt="" />
                 <i className="fa fa-check"></i>
               </Link>
             </div>
-            <div className="de_countdown">5h 30m 32s</div>
+            <div className="de_countdown">{hours}h {minutes}m {seconds}s</div>
 
             <div className="nft__item_wrap">
               <div className="nft__item_extra">
@@ -52,28 +127,32 @@ const ExploreItems = () => {
                 </div>
               </div>
               <Link to="/item-details">
-                <img src={nftImage} className="lazy nft__item_preview" alt="" />
+                <img src={nft.nftImage} className="lazy nft__item_preview" alt="" />
               </Link>
             </div>
             <div className="nft__item_info">
               <Link to="/item-details">
-                <h4>Pinky Ocean</h4>
+                <h4>{nft.title}</h4>
               </Link>
-              <div className="nft__item_price">1.74 ETH</div>
+              <div className="nft__item_price">{`${nft.price}`} ETH</div>
               <div className="nft__item_like">
                 <i className="fa fa-heart"></i>
-                <span>69</span>
+                <span>{nft.likes}</span>
               </div>
             </div>
           </div>
         </div>
       ))}
+       </>
+    )}
       <div className="col-md-12 text-center">
-        <Link to="" id="loadmore" className="btn-main lead">
+      {visibleItems < localNftData.length && (
+        <Link to="#" id="loadmore" className="btn-main lead" onClick={loadMore}>
           Load more
         </Link>
+      )}
       </div>
-    </>
+      </>
   );
 };
 
