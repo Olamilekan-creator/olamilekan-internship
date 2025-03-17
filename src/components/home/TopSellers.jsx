370b01284324aac04ca47c0dc7919dc880b491ea
Olamilekan <olamilekan.akinlusi@icloud.com>
2025-03-17 08:02:52 -0500

    topsellers dynamic api

--- src/components/home/TopSellers.jsx
+++ src/components/home/TopSellers.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React,{ useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
+import axios from 'axios';
 
 const TopSellers = () => {
+  const [localNftData, setLocalNftData] = useState([]);
+
+useEffect(() => {
+  const fetchNFTs = async () => {
+    try {
+      const response = await axios.get(
+        "https://us-central1-nft-cloud-functions.cloudfunctions.net/topSellers"
+      );
+      setLocalNftData(response.data);
+    } catch (error) {
+      console.error("There was an error fetching the NFT data!", error);
+    }
+  }
+  fetchNFTs();
+}, []);
+
   return (
     <section id="section-popular" className="pb-5">
       <div className="container">
