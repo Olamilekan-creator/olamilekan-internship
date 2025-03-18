03969e42a503bd0d562527b7c522140b0edb71bd
Olamilekan <olamilekan.akinlusi@icloud.com>
2025-03-18 15:35:37 -0500

    author api

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -1,68 +1,90 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AuthorBanner from "../images/author_banner.jpg";
 import AuthorItems from "../components/author/AuthorItems";
 import { Link } from "react-router-dom";
 import AuthorImage from "../images/author_thumbnail.jpg";
+import axios from "axios";
 
-const Author = () => {
+const Author = ({ nftData }) => {
+  const [localNftData, setLocalNftData] = useState([]);
+
+  useEffect(() => {
+    const fetchNFTs = async () => {
+      try {
+        const response = await axios.get(
+          "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors"
+        );
+        setLocalNftData(response.data);
+      } catch (error) {
+        console.error("There was an error fetching the NFT data!", error);
+      }
+    };
+    fetchNFTs();
+  }, []);
+
   return (
-    <div id="wrapper">
-      <div className="no-bottom no-top" id="content">
-        <div id="top"></div>
+    <>
+      <div id="wrapper">
+        <div className="no-bottom no-top" id="content">
+          <div id="top"></div>
+          <section
+            id="profile_banner"
+            aria-label="section"
+            className="text-light"
+            data-bgimage="url(images/author_banner.jpg) top"
+            style={{ background: `url(${AuthorBanner}) top` }}
+          ></section>
 
-        <section
-          id="profile_banner"
-          aria-label="section"
-          className="text-light"
-          data-bgimage="url(images/author_banner.jpg) top"
-          style={{ background: `url(${AuthorBanner}) top` }}
-        ></section>
+         
+              <section aria-label="section">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-12">
+                      <div className="d_profile de-flex">
+                        <div className="de-flex-col">
+                          <div className="profile_avatar">
+                            <img src={AuthorImage} alt="" />
+                            <i className="fa fa-check"></i>
+                            <div className="profile_name">
+                              <h4>
+                                Monica Lucas
+                                <span className="profile_username">
+                                  @monicaaaa
+                                </span>
+                                <span id="wallet" className="profile_wallet">
+                                  UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                                </span>
+                                <button id="btn_copy" title="Copy Text">
+                                  Copy
+                                </button>
+                              </h4>
+                            </div>
+                          </div>
+                          <div className="profile_follow de-flex">
+                            <div className="de-flex-col">
+                              <div className="profile_follower">
+                                573 followers
+                              </div>
+                              <Link to="#" className="btn-main">
+                                Follow
+                              </Link>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
 
-        <section aria-label="section">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12">
-                <div className="d_profile de-flex">
-                  <div className="de-flex-col">
-                    <div className="profile_avatar">
-                      <img src={AuthorImage} alt="" />
-
-                      <i className="fa fa-check"></i>
-                      <div className="profile_name">
-                        <h4>
-                          Monica Lucas
-                          <span className="profile_username">@monicaaaa</span>
-                          <span id="wallet" className="profile_wallet">
-                            UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                          </span>
-                          <button id="btn_copy" title="Copy Text">
-                            Copy
-                          </button>
-                        </h4>
+                    <div className="col-md-12">
+                      <div className="de_tab tab_simple">
+                        <AuthorItems />
                       </div>
                     </div>
                   </div>
-                  <div className="profile_follow de-flex">
-                    <div className="de-flex-col">
-                      <div className="profile_follower">573 followers</div>
-                      <Link to="#" className="btn-main">
-                        Follow
-                      </Link>
-                    </div>
-                  </div>
                 </div>
-              </div>
-
-              <div className="col-md-12">
-                <div className="de_tab tab_simple">
-                  <AuthorItems />
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+              </section>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
