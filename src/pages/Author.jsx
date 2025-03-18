a1dec4738a22d7c0b083eff3f1e95bc7aa5d927a
Olamilekan <olamilekan.akinlusi@icloud.com>
2025-03-18 16:01:15 -0500

    author api changes

--- src/pages/Author.jsx
+++ src/pages/Author.jsx
@@ -12,7 +12,7 @@ const Author = ({ nftData }) => {
     const fetchNFTs = async () => {
       try {
         const response = await axios.get(
-          "https://us-central1-nft-cloud-functions.cloudfunctions.net/authors"
+          `https://us-central1-nft-cloud-functions.cloudfunctions.net/authors=${author}`
         );
         setLocalNftData(response.data);
       } catch (error) {
@@ -35,53 +35,48 @@ const Author = ({ nftData }) => {
             style={{ background: `url(${AuthorBanner}) top` }}
           ></section>
 
-         
-              <section aria-label="section">
-                <div className="container">
-                  <div className="row">
-                    <div className="col-md-12">
-                      <div className="d_profile de-flex">
-                        <div className="de-flex-col">
-                          <div className="profile_avatar">
-                            <img src={AuthorImage} alt="" />
-                            <i className="fa fa-check"></i>
-                            <div className="profile_name">
-                              <h4>
-                                Monica Lucas
-                                <span className="profile_username">
-                                  @monicaaaa
-                                </span>
-                                <span id="wallet" className="profile_wallet">
-                                  UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
-                                </span>
-                                <button id="btn_copy" title="Copy Text">
-                                  Copy
-                                </button>
-                              </h4>
-                            </div>
-                          </div>
-                          <div className="profile_follow de-flex">
-                            <div className="de-flex-col">
-                              <div className="profile_follower">
-                                573 followers
-                              </div>
-                              <Link to="#" className="btn-main">
-                                Follow
-                              </Link>
-                            </div>
-                          </div>
+          <section aria-label="section">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="d_profile de-flex">
+                    <div className="de-flex-col">
+                      <div className="profile_avatar">
+                        <img src={AuthorImage} alt="" />
+                        <i className="fa fa-check"></i>
+                        <div className="profile_name">
+                          <h4>
+                            Monica Lucas
+                            <span className="profile_username">@monicaaaa</span>
+                            <span id="wallet" className="profile_wallet">
+                              UDHUHWudhwd78wdt7edb32uidbwyuidhg7wUHIFUHWewiqdj87dy7
+                            </span>
+                            <button id="btn_copy" title="Copy Text">
+                              Copy
+                            </button>
+                          </h4>
                         </div>
                       </div>
-                    </div>
-
-                    <div className="col-md-12">
-                      <div className="de_tab tab_simple">
-                        <AuthorItems />
+                      <div className="profile_follow de-flex">
+                        <div className="de-flex-col">
+                          <div className="profile_follower">573 followers</div>
+                          <Link to="#" className="btn-main">
+                            Follow
+                          </Link>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </section>
+
+                <div className="col-md-12">
+                  <div className="de_tab tab_simple">
+                    <AuthorItems />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
         </div>
       </div>
     </>
