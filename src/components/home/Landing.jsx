4ef6ec6bff0a502d270ee9eec456b3986b99d0a7
Olamilekan <olamilekan.akinlusi@icloud.com>
2025-03-21 22:17:30 -0500

    landing animations

--- src/components/home/Landing.jsx
+++ src/components/home/Landing.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NFT from "../../images/nft.png";
 import backgroundImage from "../../images/bg-shape-1.jpg";
 import { Link } from "react-router-dom";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Landing = () => {
+
+    useEffect(() => {
+      window.scrollTo(0, 0);
+      AOS.init();
+    }, []);
+    
+    document.addEventListener('aos:in', ({ detail }) => {
+      console.log('animated in', detail);
+    });
+    
+    document.addEventListener('aos:out', ({ detail }) => {
+      console.log('animated out', detail);
+    });
+  
+
   return (
     <section
       id="section-hero"
@@ -16,27 +33,27 @@ const Landing = () => {
         <div className="container">
           <div className="row align-items-center">
             <div className="col-md-6">
-              <div className="spacer-single"></div>
+              <div className="spacer-single" ></div>
               <h6>
                 <span className="text-uppercase id-color-2">
                   Ultraverse Market
                 </span>
               </h6>
               <div className="spacer-10"></div>
-              <h1>Create, sell or collect digital items.</h1>
-              <p className="lead">
+              <h1 data-aos="fade-up">Create, sell or collect digital items.</h1>
+              <p className="lead" data-aos="fade-up" data-aos-delay="100">
                 Unit of data stored on a digital ledger, called a blockchain,
                 that certifies a digital asset to be unique and therefore not
                 interchangeable
               </p>
-              <div className="spacer-10"></div>
-              <Link className="btn-main lead" to="/explore">
+              <div className="spacer-10" ></div>
+              <Link className="btn-main lead" to="/explore" data-aos="fade-up" data-aos-delay="400">
                 Explore
               </Link>
               <div className="mb-sm-30"></div>
             </div>
             <div className="col-md-6 xs-hide">
-              <img src={NFT} className="lazy img-fluid" alt="" />
+              <img src={NFT} className="lazy img-fluid" alt="" data-aos="fade-in" data-aos-delay="200"/>
             </div>
           </div>
         </div>
